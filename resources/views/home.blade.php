88129b12ac5d6becef6a1b5e42d97806fcbc8b0a
Thiago Lopes <thiagoslopes8@outlook.com>
2026-03-10 18:23:05 -0300

    feat(home): use Chirp model

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,25 @@
 <x-layout>
-    <x-slot:title>Welcome</x-slot:title>
+    <x-slot:title>
+        Welcome
+    </x-slot:title>
+
     <main class="flex-1 container mx-auto px-4 py-8">
-        @foreach ($chirps as $chirp)
-            <div class="max-w-2xl mx-auto">
-                <div class="card bg-base-100 shadow mt-8">
-                    <div class="card-body">
-                        <div>
-                            <div class="font-semibold">{{ $chirp['author'] }}</div>
-                            <div class="mt-1">{{ $chirp['message'] }}</div>
-                            <div class="text-sm text-gray-500 mt-2">{{ $chirp['time'] }}</div>
+    <div class="max-w-2xl mx-auto">
+        @forelse ($chirps as $chirp)
+            <div class="card bg-base-100 shadow mt-8">
+                <div class="card-body">
+                    <div>
+                        <div class="font-semibold"> {{ $chirp->user ? $chirp->user->name : 'Anonymous' }}</div>
+                        <div class="mt-1">{{ $chirp->message }}</div>
+                        <div class="text-sm text-gray-500 mt-2">
+                            {{ $chirp->created_at->diffForHumans() }}
                         </div>
                     </div>
                 </div>
             </div>
-        @endforeach
+        @empty
+            <p class="text-gray-500">No chirps yet. Be the first to chirp!</p>
+        @endforelse
+    </div>
     </main>
 </x-layout>
