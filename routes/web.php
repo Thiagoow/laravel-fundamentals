74b0f62a032e1f004500ec54a5f69c73c7dff1d6
Thiago Lopes <thiagoslopes8@outlook.com>
2026-03-11 16:28:49 -0300

    feat(auth): add register controller

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\Auth\Logout;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChirpController;
 
 Route::get('/', [ChirpController::class, 'index']);
 Route::resource('chirps', ChirpController::class)->except(['index', 'create', 'show']);
 
-Route::view('/register', 'auth.register');
+Route::view('/register', 'auth.register')->middleware('guest');
+Route::post('/register', Register::class)->middleware('guest');
